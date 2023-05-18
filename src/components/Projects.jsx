1034d1680e448c7f81009a889b423fe65c677799
Kszalank <szalankiewiczkrzysiek@gmail.com>
2023-05-18 16:58:38 +0200

    websites address changed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ function Projects() {
           name="Weather App"
           description="Application showing weather in every single city in the World."
           technologies="Javascript, SASS, HTML, Node.js, Prettier."
-          web="https://singular-gumdrop-23382d.netlify.app"
+          web="https://weatherapp-kszalank.netlify.app"
           github="https://github.com/Kszalank/WeatherApp"
         />
 
@@ -27,7 +27,7 @@ function Projects() {
           name="Currency Converter"
           description="Web application converting all known currencies thanks to the usage of API data."
           technologies="React, Javascript, SASS, HTML,  Node.js, Yarn, Prettier, Eslint."
-          web="https://tiny-empanada-89bbfb.netlify.app/?fbclid=IwAR2S6fLr3gP9Mv-uibbmzETe31IeD3u4o95v30hWLVeZhzfY4XsBCxC5zpE"
+          web="https://currency-converter-kszalank.netlify.app"
           github="https://github.com/Kszalank/Currency-Converter"
         />
       </div>
